378563bf5f33b52d144bb7f3bf9c5a703eb36c06
ayoubfoxy <126797520+ayoubfoxy@users.noreply.github.com>
2023-03-08 15:50:33 +0000

    Update main.py

--- main.py
+++ main.py
@@ -408,7 +408,7 @@ class Proxy:
 
 
             s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            #s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
             s.bind((host, port))
             s.listen()
 
