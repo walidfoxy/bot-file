2d9327c38b2ca2eaae0c9e2a7ff7678e742f4d8c
ayoubfoxy <126797520+ayoubfoxy@users.noreply.github.com>
2023-03-08 15:16:55 +0000

    Update main.py

--- main.py
+++ main.py
@@ -715,7 +715,7 @@ class Proxy:
 
 def start_bot():
     try :
-        Proxy().runs('192.168.1.38',3000)
+        Proxy().runs('127.0.0.1',3000)
     except Exception as e:
         restart()
         sea=2
