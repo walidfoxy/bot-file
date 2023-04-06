15c0f48662c6c8e6f27fc1e2f4da44eb160f13ad
ayoubfoxy <126797520+ayoubfoxy@users.noreply.github.com>
2023-04-06 16:40:57 +0100

    Update main.py

--- main.py
+++ main.py
@@ -842,8 +842,8 @@ class Proxy:
                                     if b"/back" in dataS:
                                         back=True
                                         threading.Thread(target=self.sqd , args=(self.data_join,)).start()
-                                        client.send(bytes.fromhex(gen_msgv2(dataS.hex() ,"[00FF00][b][c]activ ok!")))
-                                        client.send(bytes.fromhex(str(gen_msgv2_clan(dataS.hex() ,"[00FF00][b][c]activ ok!"))))
+                                        client.send(bytes.fromhex(gen_msgv2(dataS.hex() ,"[00FF00][b][c]sss ok!")))
+                                        client.send(bytes.fromhex(str(gen_msgv2_clan(dataS.hex() ,"[00FF00][b][c]sss ok!"))))
 
 
                                     statues= False
@@ -945,7 +945,7 @@ class Proxy:
                 self.op.send(data_join)
                 time.sleep(900.9)
                
-                #                           0515000001d0dd55f9338d6bd
+                #                           050000030b08e7b0beb20a100520062afe0508e7b0beb20a1
 
             except Exception as e:
                 
